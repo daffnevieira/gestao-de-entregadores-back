fdd8060f63750e52f6b8cb17e5345d7b11ee65e7
Daffne <seuemail@exemplo.com>
2025-04-07 22:24:19 -0300

    Estrutura inicial do projeto

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
